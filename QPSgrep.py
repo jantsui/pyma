2720d5fae5cd05e5a49a5d0b12c191a31deb31ed
xujian <xujian@USER-20180212RO.stt.com>
2018-10-17 14:23:04 +0800

    Queries

--- QPSgrep.py
+++ QPSgrep.py
@@ -3,11 +3,11 @@ import re
 Com_commit_S = """Com_commit""" #4140
 Com_rollback_S = """Com_rollback""" #4197
 #执行查询的总次
-Questions_S = """Questions""" #4415
+Questions_S = """Queries""" #4415
 #服务器已经运行的时间（以秒为单位）
 my_Uptime = """Uptime""" #4470
 
-f = open("H:\\IOE\\监控脚本\\resdb-01\\20180620_resdb-01_check.txt","r",encoding="utf-8")
+f = open("H:\\IOE\\监控脚本\\resdb-01\\20181016_resdb-01_check.txt","r",encoding="utf-8")
 
 Com_rollback_T = 0 
 for line in f:
@@ -17,7 +17,7 @@ for line in f:
     if  re.search( Com_rollback_S , line ):
         if Com_rollback_T==0:
             Com_rollback_T=int(re.search("(?P<name>\D+)(?P<value>\d+)",line).group("value"))
-        print(Com_rollback_T)
+            print(Com_rollback_T)
     if Questions_S in line:
         Questions_T=int(re.search("(?P<name>\D+)(?P<value>\d+)",line).group("value"))
         print(Questions_T)
